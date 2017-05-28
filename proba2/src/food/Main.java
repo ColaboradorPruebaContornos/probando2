fed301f9ba8fc76bcfffa81d7f7f6443d092fb1d
ColaboradorPruebaContornos <laustinta@gmail.com>
2017-05-29 00:39:24 +0200

    Add files via upload

---
+++ proba2/src/food/Main.java
@@ -0,0 +1,43 @@
+package food;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+/**
+ * 
+ * @author David Ordóñez Valiña
+ */
+class Main {
+ 
+     public static void main(String[] args) {
+      
+         Random rd = new Random();
+         int a;
+         String asasdasda="si o que!";
+       String sdsa ="Hola. Soy el colaborador";
+        int [] numeros = new int [2];
+        int [] todos = new int [12];
+         
+         for (int i = 0; i < numeros.length; i++) {
+             a= rd.nextInt(12)+1;
+             if (todos[a-1]==0) {
+                 numeros[i]=a;
+             }
+             else {
+                 a= rd.nextInt(50)+1;
+             if (todos[a-1]==0) {
+                 numeros[i]=a;
+             }
+             }
+                 
+             
+             
+         }
+         System.out.println(Arrays.toString(numeros));
+    }
+  
+             
+     
+    
+}
